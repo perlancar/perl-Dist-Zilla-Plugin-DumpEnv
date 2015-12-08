a2d7328b60e692a7b67b2d243f1e1828a5c762d0
perlancar (@pc-office) <perlancar@gmail.com>
2015-12-08 16:03:28 +0700

    Initial commit

---
+++ lib/Dist/Zilla/Plugin/DumpEnv.pm
@@ -0,0 +1,39 @@
+package Dist::Zilla::Plugin::DumpEnv;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+use Moose;
+with (
+    'Dist::Zilla::Role::BeforeBuild',
+);
+
+use namespace::autoclean;
+
+sub before_build {
+    my $self = shift;
+    $self->log(["ENV: %s", \%ENV]);
+}
+
+__PACKAGE__->meta->make_immutable;
+1;
+# ABSTRACT: Dump %ENV in Before Build phase
+
+=for Pod::Coverage .+
+
+=head1 SYNOPSIS
+
+In F<dist.ini>:
+
+ [DumpEnv]
+
+
+=head1 DESCRIPTION
+
+=head1 SEE ALSO
+
+=cut
